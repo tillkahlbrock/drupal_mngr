7f32edfd60026cb29467d4b4d4b00ab79bbb1b8e
Till Kahlbrock <till@jimdo.com>
2013-02-28 12:26:56 +0100

    adds filesystem check for path

--- spec/DrupalMngr/VersionFinder.php
+++ spec/DrupalMngr/VersionFinder.php
@@ -3,11 +3,26 @@
 namespace spec\DrupalMngr;
 
 use PHPSpec2\ObjectBehavior;
+use Symfony\Component\Filesystem\Filesystem;
 
 class VersionFinder extends ObjectBehavior
 {
-    function it_should_be_initializable()
+
+    function let($filesystem)
     {
-        $this->shouldHaveType('DrupalMngr\VersionFinder');
+        $filesystem->beAMockOf('Symfony\Component\Filesystem\Filesystem');
+        $this->beConstructedWith($filesystem);
+    }
+
+    /**
+     * @param $filesystem Filesystem
+     */
+    function it_should_use_filesystem_to_check_if_the_path_exists($filesystem)
+    {
+        $path = 'some/path';
+
+        $filesystem->exists($path)->shouldBeCalled();
+
+        $this->getCurrentVersion($path);
     }
 }
--- src/DrupalMngr/VersionFinder.php
+++ src/DrupalMngr/VersionFinder.php
@@ -1,7 +1,19 @@
 <?php
 
 namespace DrupalMngr;
+use Symfony\Component\Filesystem\Filesystem;
 
 class VersionFinder
 {
+    private $filesystem;
+
+    public function __construct(Filesystem $filesystem)
+    {
+        $this->filesystem = $filesystem;
+    }
+
+    public function getCurrentVersion($path)
+    {
+        $this->filesystem->exists($path);
+    }
 }
